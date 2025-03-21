a09d0a5e6ec4b0fff0617eb2f3c1ee8d101e999d
Noah34a <noah.lemoing@ynov.com>
2025-03-21 11:59:22 +0100

    UserController

--- src/controllers/UserController.java
+++ src/controllers/UserController.java
@@ -1,5 +1,39 @@
-package controllers;
+import java.util.*;
 
 public class UserController {
-    
+    private UserService userService;
+
+    public UserController() {
+        this.userService = new UserService();
+    }
+
+    // Enregistrer un nouvel utilisateur
+    public void registerUser(String username, String email, String password) {
+        User newUser = new User(username, email, password);
+        userService.registerUser(newUser);
+        System.out.println("Utilisateur enregistré avec succès.");
+    }
+
+    // Se connecter avec un utilisateur
+    public boolean loginUser(String email, String password) {
+        User user = userService.authenticateUser(email, password);
+        if (user != null) {
+            System.out.println("Connexion réussie.");
+            return true;
+        }
+        System.out.println("Identifiants invalides.");
+        return false;
+    }
+
+    // Voir l'historique des commandes
+    public void viewOrderHistory(int userId) {
+        List<Order> orderHistory = userService.getOrderHistory(userId);
+        if (orderHistory.isEmpty()) {
+            System.out.println("Aucune commande trouvée.");
+        } else {
+            for (Order order : orderHistory) {
+                System.out.println(order.getOrderDetails());
+            }
+        }
+    }
 }
