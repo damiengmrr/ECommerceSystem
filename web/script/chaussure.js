3fa08061634918a7946825461d78369272a790aa
DOMBALLE <theodom974>
2025-03-26 09:54:49 +0100

    Fin

--- web/script/chaussure.js
+++ web/script/chaussure.js
@@ -1,9 +1,9 @@
 const products = [
     { id: 1, name: "Sneaker B35 NXXT", price: 1200, image: "image/chaussure/chaussure1.png" },
-    { id: 2, name: "Mocassin Dior Granville", price: 1000, image: "image/chaussure/chaussure2.png" },
-    { id: 3, name: "Richelieu Dior Embassy", price: 500, image: "image/chaussure/chaussure3.png" },
-    { id: 4, name: "Sneaker B30 Countdown", price: 950, image: "image/chaussure/b30.png" },
-    { id: 5, name: "Sneaker B44 Blade DIOR AND LEWIS HAMILTON – ÉDITION LIMITÉE ET NUMÉROTÉE", price: 900, image: "image/chaussure/b44.png" },
+    { id: 2, name: "Mocassin Dior ", price: 1000, image: "image/chaussure/chaussure2.png" },
+    { id: 3, name: "Richelieu Dior ", price: 500, image: "image/chaussure/chaussure3.png" },
+    { id: 4, name: " B30 Countdown", price: 950, image: "image/chaussure/b30.png" },
+    { id: 5, name: " B44 Blade DIOR ", price: 900, image: "image/chaussure/b44.png" },
 ];
 
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
